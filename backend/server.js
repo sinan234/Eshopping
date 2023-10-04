936be97369edbda0e83f92ebddbfd077d15991ac
Mohammed Sinan <sinan.m@thinkpalm.com>
2023-10-04 18:45:39 +0530

    session working

--- backend/server.js
+++ backend/server.js
@@ -13,23 +13,14 @@ const api= require('./routes/api');
 const sessionStore = {};
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
-const sessionTimeout = 20 * 1 * 1000;
+const sessionTimeout = 300 * 1 * 1000;
 db.connect()
   
 // app.use('/', api);
 // app.use('/about', api);
 app.use(cors());
 app.use(cookieParser());
-app.use(session({
-  secret: 'secretKey',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: sessionTimeout,
-    secure: true, 
-    httpOnly: true 
-  }
-}));
+
 app.use(bodyParser.json());
 
 app.get('/',  function (req, res) {
@@ -44,6 +35,29 @@ app.get('/special', verifyToken,function (req, res) {
   ]
   res.json(specialEvents);
 });
+
+// app.get('/user', (req, res) => {
+//   try {
+//     const token = req.headers.authorization.split(' ')[1];
+//     const decodedToken = jwt.verify(token, 'secretKey');
+//     const userId = decodedToken.subject;
+
+//     // Retrieve user information based on the userId
+//     // Replace this with your own implementation to fetch user data from the database
+//     const user = User.findById(userId);
+
+//     if (!user) {
+//       return res.status(404).json({ message: 'User not found' });
+//     }
+
+//     // Return the user information
+//     res.status(200).json(user);
+//   } catch (error) {
+//     console.log('Error retrieving user information:', error);
+//     return res.status(500).json({ message: 'Error retrieving user information' });
+//   }
+// });
+
 
 app.post('/create_user', async(req, res) => {
   try {
@@ -65,13 +79,19 @@ app.post('/create_wishlist', async (req, res) => {
    try{
     
     const{product_name,product_id,product_price,product_image, product_available} = req.body;
+    const token = req.headers.authorization.split(' ')[1];
+    console.log('Token:', token);
+    const user = await User.findOne({ token });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
     const product = await Productwish.findOne({product_id});
     if(product){
       console.log('Product already exists');
       return res.status(400).json({message: "Product already exists"});
     }
     const newProduct = new Productwish({product_name,product_id,product_price,product_image, product_available});
-    newProduct.save();
+    await newProduct.save();
     console.log('Product added:', newProduct);
     res.status(201).json({message: "Product added successfully"});
    } catch(error){
@@ -105,11 +125,13 @@ app.delete('/remove_wishlist', async (req, res) => {
 
 });
 
+
+
 app.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    
+
     if (!user) {
       console.log('User not found');
       return res.status(400).json({ message: "User not found" });
@@ -120,34 +142,34 @@ app.post('/login', async (req, res) => {
     if (!isPasswordValid) {
       return res.status(401).json({ message: "Invalid password" });
     }
+
     const session = {
       userId: user._id,
       expiry: Date.now() + sessionTimeout
+      // Add any other properties to the session object as needed
     };
-    let payload ={subject :  user._id}
-    let token= jwt.sign(payload, 'secretKey');
-    // sessionStore[token] = session;
-    
-    req.sessionStore[token] = session;
-    console.log(req.session) 
-   
 
-    res.cookie('userToken', token, {
+    const token = jwt.sign(session, 'secretKey');
+    user.token = token; // Store the token in the user document
+await user.save();
+    const cookieValue = {
+      token,
+      sessionIndicator: true,
+      sessionEnd: session.expiry
+    };
+
+    res.cookie('userCookie', JSON.stringify(cookieValue), {
       maxAge: sessionTimeout,
       secure: true,
       httpOnly: true
     });
-    const userToken = req.cookies.userToken;
-    console.log(userToken)
-  
-    res.status(201).json({ message: "Authentication successful", token });
-    
+    res.status(201).json({ message: "Authentication successful" , cookie: cookieValue});
+
   } catch (error) {
     console.log('Error logging in:', error);
     return res.status(500).json({ message: "Error logging in" });
   }
 });
-
 
 function verifyToken(req, res, next) {
   if (!req.headers.authorization) {
